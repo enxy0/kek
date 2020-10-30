91773dd9f90029ba80055c48944688dcd75e4ae0
enxy0 <enxyox@gmail.com>
2020-10-31 00:37:35 +0300

    :fire: Add decoder, fix algo

--- KotlinRLE/src/main/kotlin/Main.kt
+++ KotlinRLE/src/main/kotlin/Main.kt
@@ -1,45 +1,73 @@
 /**
- * Реализация алгоритма RLE (Run-Length Encoding) - алгоритм сжатия, заменяющий идущие подряд одинаковые символы парой
- * (повторяющийся символ, количество повторений).
+ * Реализация алгоритма RLE (Run-Length Encoding)
+ *
+ * Количество решенных задач: 1
+ * Общее количество задач: 1
  *
  * Автор: Бушуев Никита Федорович
- * Дата: 09.10.2020
+ * Дата: 30.10.2020
+ * Kotlin: 1.4.10 (jvm)
  */
 
 fun main() {
-    println("test = ${test()}")
+    runTests()
 }
 
-fun test(): Boolean {
-    println("RLE(\"aabbbccda\") = ${runLengthEncoding("aabbbccda")}")
-    println("RLE(\"abcddcba\") = ${runLengthEncoding("abcddcba")}")
-    println("RLE(\"abaacaaa\") = ${runLengthEncoding("abaacaaa")}")
-    println("RLE(\"aaaaaaaa\") = ${runLengthEncoding("aaaaaaaa")}")
-    println("RLE(\"\") = ${runLengthEncoding("")}")
-    return runLengthEncoding("aabbbccda") == "(a, 2) (b, 3) (c, 2) (d, 1) (a, 1)"
-            && runLengthEncoding("abcddcba") == "(a, 1) (b, 1) (c, 1) (d, 2) (c, 1) (b, 1) (a, 1)"
-            && runLengthEncoding("abaacaaa") == "(a, 1) (b, 1) (a, 2) (c, 1) (a, 3)"
-            && runLengthEncoding("aaaaaaaa") == "(a, 8)"
-            && runLengthEncoding("") == ""
-
-}
-
-fun runLengthEncoding(text: String): String {
+/**
+ * Кодирует text с помощью RLE алгоритма
+ */
+fun encodeRle(text: String): String {
     var last: Char = text.firstOrNull() ?: return ""
     var counter = 1
     var result = ""
     for (i in 1 until text.length) {
         if (last != text[i]) {
-            result += result.withSpacing("($last, $counter)")
+            result += toStringPair(last, counter).plus(" ")
             counter = 0
             last = text[i]
         }
         counter++
     }
-    if (counter != 0) result += result.withSpacing("($last, $counter)")
+    if (counter != 0) result += toStringPair(last, counter)
     return result
 }
 
-fun String.withSpacing(value: String): String {
-    return if (this.isEmpty()) value else " $value"
+/**
+ * Декодирует text из RLE
+ */
+fun decodeRle(text: String): String = if (text.isNotEmpty()) {
+    text.split(' ')
+        .map { it[1].toString() to it[3].toString().toInt() }
+        .fold("") { acc, (letter, counter) ->
+            acc + letter.repeat(counter)
+        }
+} else ""
+
+/**
+ * Запускает серию тестов с выводом результата в консоль
+ */
+fun runTests() {
+    test("")
+    test("abc")
+    test("abccdd")
+    test("aabbbccda")
+    test("abcddcba")
+    test("abaacaaa")
+    test("aaaaaaaa")
 }
+
+/**
+ * Запускает тест для конкретного значения
+ */
+fun test(text: String): Boolean {
+    val encodedText = encodeRle(text)
+    val decodedText = decodeRle(encodedText)
+    val isCorrect = text == decodedText
+    println("\"$text\" = \"$encodedText\" : $isCorrect")
+    return isCorrect
+}
+
+/**
+ * Строковое представление пары из буквы и количества ее повторений - (a, 4)
+ */
+fun toStringPair(letter: Char, counter: Int) = "($letter,$counter)"
